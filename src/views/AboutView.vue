76d0aed6eaa20d465fe8efb240dee25a6060dca7
utkirbek <utkirbektoirov1@gmail.com>
2024-01-18 12:41:12 +0500

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,12 +2,17 @@
   <div class="about">
     <h1>This is an about page</h1>
     <div class="cards">
-      <button id="button1">button1</button>
+      <button id="button1" @click="onClose()">button1</button>
     </div>
   </div>
 </template>
 
 <script setup>
+const tg = window.Telegram.WebApp;
+
+const onClose = () => {
+  tg.close();
+};
 // document.getElementById("button1").addEventListener("click", function () {
 //   Telegram.WebApp.sendData("BUTTON1");
 // });
