62b9947f2487a523b2bc408de9dc7563964c4048
utkirbek <utkirbektoirov1@gmail.com>
2024-01-18 11:53:03 +0500

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="cards">
+      <button id="button1">button1</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+// document.getElementById("button1").addEventListener("click", function () {
+//   Telegram.WebApp.sendData("BUTTON1");
+// });
+</script>
 
 <style>
 @media (min-width: 1024px) {
