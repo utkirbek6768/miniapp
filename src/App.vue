dff3e93a537a81288ddbdfcb6b19a30b3072ab39
utkirbek <utkirbektoirov1@gmail.com>
2024-01-19 18:10:08 +0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import { watchEffect } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Wellcome to Yalla" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -19,7 +26,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView />
 </template>
-
+<script setup>
+watchEffect(() => {});
+</script>
 <style scoped>
 header {
   line-height: 1.5;
