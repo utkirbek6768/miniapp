2169a03db3c439d018a9b17ebdb46e52f5ebd5e2
utkirbek <utkirbektoirov1@gmail.com>
2024-01-20 13:18:20 +0500

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,47 @@
 <script setup>
+// import { onToggleButton } from "../hooks/userTelegram.js";
+import { watchEffect, ref } from "vue";
 const tg = window.Telegram.WebApp;
 
-const onClose = () => {
-  tg.close();
-};
+const form = ref({
+  name: "",
+  age: null,
+});
 
-const onToggleButton = () => {
-  if (tg.MainButton.isVisible) {
+// const onClose = () => {
+//   tg.close();
+// };
+
+const onSendData = useCallback(() => {
+  form;
+});
+watchEffect(() => {
+  if (!form.name || !form.age) {
     tg.MainButton.hide();
   } else {
     tg.MainButton.show();
   }
-};
+});
+watchEffect(() => {
+  tg.MainButton.setParams({
+    text: "Malumotlarni yuborish",
+  });
+});
+watchEffect(() => {
+  Telegram.WebApp.onEvent(mainButtonClicked, onSendData);
+});
 </script>
 
 <template>
   <main>
     <div class="cards">
-      <button @click="onClose()" class="button">Saytni yopish</button>
+      <!-- <button @click="onClose()" class="button">Saytni yopish</button> -->
 
-      <span>{{ tg?.initDataUnsafe?.user?.username }}</span>
+      <!-- <span>{{ tg?.initDataUnsafe?.user?.username }}</span> -->
 
-      <button @click="onToggleButton()" class="button">onToggleButton</button>
-
-      <form class="form">
-        <input type="text" class="input" />
-        <input type="text" class="input" />
-        <input type="text" class="input" />
+      <form :model="form" class="form">
+        <input v-model="form.name" type="text" class="input" />
+        <input v-model="form.age" type="number" class="input" />
         <input type="submit" class="input" />
       </form>
     </div>
