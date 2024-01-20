6a1658ce7da73ecb27e9c11720efa6fe4a9b1ee8
utkirbek <utkirbektoirov1@gmail.com>
2024-01-20 18:03:58 +0500

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,15 +23,27 @@ const handleMainButtonClicked = (eventData) => {
   }
 };
 
+const sendMsg = () => {
+  const botToken = "6978212908:AAEjdFxJgAWe3ToUT-cz6qhjot-8qkUqIRU";
+  const chatId = 177482674;
+  const message = "6978212908:AAEjdFxJgAWe3ToUT-cz6qhjot-8qkUqIRU";
+  const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}`;
+  fetch(apiUrl, { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Message sent:", data);
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+    });
+};
+
 // Other script setup code if needed
 
 // Attach the event listener when the component is mounted
 onMounted(() => {
   if (window.Telegram && window.Telegram.WebApp) {
-    window.Telegram.WebApp.onEvent(
-      "mainButtonClicked",
-      handleMainButtonClicked
-    );
+    window.Telegram.WebApp.onEvent("mainButtonClicked", sendMsg);
   } else {
     console.error("Telegram WebApp library is not loaded.");
   }
@@ -40,10 +52,7 @@ onMounted(() => {
 // Remove the event listener when the component is unmounted
 onUnmounted(() => {
   if (window.Telegram && window.Telegram.WebApp) {
-    window.Telegram.WebApp.offEvent(
-      "mainButtonClicked",
-      handleMainButtonClicked
-    );
+    window.Telegram.WebApp.offEvent("mainButtonClicked", sendMsg);
   } else {
     console.error("Telegram WebApp library is not loaded.");
   }
