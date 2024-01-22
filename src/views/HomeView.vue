b60d23e7069d7a915450c6228fedff899bfa94eb
utkirbek <utkirbektoirov1@gmail.com>
2024-01-22 14:16:53 +0500

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <main>
-    <button @click="onToggleButton" class="button">Toggle Button</button>
     <button @click="onSendData" class="button">Send Data</button>
 
     <form :model="form" class="form">
@@ -11,7 +10,6 @@
         class="input"
         placeholder="Name"
       />
-      <span>{{ form.name }}</span>
 
       <input
         v-model="form.age"
@@ -20,7 +18,6 @@
         class="input"
         placeholder="Age"
       />
-      <span>{{ form.age }}</span>
     </form>
   </main>
 </template>
@@ -36,18 +33,7 @@ const mainButtonShow = () => {
   if (form.value.name == "" || form.value.age == "") {
     window.Telegram.WebApp.MainButton.hide();
   } else {
-    sendMsg();
     window.Telegram.WebApp.MainButton.show();
-  }
-};
-const onToggleButton = () => {
-  if (window.Telegram.WebApp) {
-    window.Telegram.WebApp.MainButton.isVisible
-      ? window.Telegram.WebApp.MainButton.hide()
-      : window.Telegram.WebApp.MainButton.show();
-    sendMsg();
-  } else {
-    console.error("Telegram WebApp library is not loaded.");
   }
 };
 
@@ -56,7 +42,6 @@ const onSendData = () => {
 };
 
 watchEffect(() => {
-  //   window.Telegram.WebApp.onEvent("mainButtonClicked", onSendData);
   if (window.Telegram.WebApp) {
     window.Telegram.WebApp.MainButton.setParams({
       text: "Malumotlarni yuborish",
@@ -67,27 +52,27 @@ watchEffect(() => {
     console.error("Telegram WebApp library is not loaded.");
   }
 });
-watch(form.value, () => {
-  if (form.value.name == "" || form.value.age == "") {
-    window.Telegram.WebApp.MainButton.hide();
-  } else {
-    window.Telegram.WebApp.MainButton.show();
-  }
-});
-const sendMsg = () => {
-  const botToken = "6978212908:AAEjdFxJgAWe3ToUT-cz6qhjot-8qkUqIRU";
-  const chatId = 177482674;
-  const message = "Main button bosildi";
-  const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}`;
-  fetch(apiUrl, { method: "GET" })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Message sent:", data);
-    })
-    .catch((error) => {
-      console.error("Error sending message:", error);
-    });
-};
+// watch(form.value, () => {
+//   if (form.value.name == "" || form.value.age == "") {
+//     window.Telegram.WebApp.MainButton.hide();
+//   } else {
+//     window.Telegram.WebApp.MainButton.show();
+//   }
+// });
+// const sendMsg = () => {
+//   const botToken = "6978212908:AAEjdFxJgAWe3ToUT-cz6qhjot-8qkUqIRU";
+//   const chatId = 177482674;
+//   const message = "Main button bosildi";
+//   const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}`;
+//   fetch(apiUrl, { method: "GET" })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("Message sent:", data);
+//     })
+//     .catch((error) => {
+//       console.error("Error sending message:", error);
+//     });
+// };
 // onUnmounted(async () => {
 //   try {
 //     window.Telegram.WebApp.offEvent("mainButtonClicked", sendMyName);
